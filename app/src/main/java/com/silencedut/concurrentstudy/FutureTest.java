715a48c55e80f82fc65d55bfb1c8e67edf31ead0
SilenceDut <silencedutchman@foxmail.com>
2016-07-18 21:33:37 +0800

    add future_test

---
+++ app/src/main/java/com/silencedut/concurrentstudy/FutureTest.java
@@ -0,0 +1,67 @@
+package com.silencedut.concurrentstudy;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+
+/**
+ * Created by SilenceDut on 16/7/18.
+ */
+
+public class FutureTest {
+    public static void main(String[] args) {
+        FutureTest futureTest = new FutureTest();
+        futureTest.useExecutor();
+        futureTest.useThread();
+    }
+
+    private void useExecutor() {
+        SumTask sumTask = new SumTask(1000);
+        ExecutorService executor = Executors.newCachedThreadPool();
+        FutureTask<Integer> futureTask = new FutureTask<Integer>(sumTask);
+        executor.submit(futureTask);
+        executor.shutdown();
+        try {
+            System.out.println("task运行结果" + futureTask.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void useThread() {
+        SumTask sumTask = new SumTask(500);
+        ExecutorService executor = Executors.newCachedThreadPool();
+        FutureTask<Integer> futureTask = new FutureTask<Integer>(sumTask);
+        Thread thread = new Thread(futureTask);
+        thread.start();
+        try {
+            System.out.println("task运行结果" + futureTask.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    class SumTask implements Callable<Integer> {
+        int number;
+        public SumTask(int num) {
+            this.number = num;
+        }
+        @Override
+        public Integer call() throws Exception {
+            System.out.println(Thread.currentThread());
+            Thread.sleep(5000);
+            int sum = 0;
+            for (int i = 0; i < number; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
--- app/src/main/java/com/silencedut/concurrentstudy/MainActivity.java
+++ app/src/main/java/com/silencedut/concurrentstudy/MainActivity.java
@@ -1,8 +1,10 @@
 package com.silencedut.concurrentstudy;
 
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
+import android.support.v4.os.AsyncTaskCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         final ThreadLocal<Integer> integerThreadLocal = new ThreadLocal<Integer>();
         integerThreadLocal.set(1);
         Log.i(TAG+Thread.currentThread(),integerThreadLocal.get()+"");
-        Looper.prepare();
+
         HandlerThread ioThread = new HandlerThread("IoThread");
         ioThread.start();
         Handler ioHandle = new Handler(ioThread.getLooper());
@@ -32,11 +34,18 @@ public class MainActivity extends AppCompatActivity {
                 //没调用start方法，没有新开线程，那个线程的 looper调用就就在哪个线程执行
             }
         });
+        for(int i = 1 ;i <= 145 ; i++)
+        {
+            new MyAsyncTask2().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        }
 
         new Thread("Thread1") {
             @Override
             public void run() {
-                Log.i(TAG+Thread.currentThread(),integerThreadLocal.get()+"");
+                 final int CPU_COUNT = Runtime.getRuntime().availableProcessors();
+                 final int CORE_POOL_SIZE = CPU_COUNT + 1;
+                 final int MAXIMUM_POOL_SIZE = CPU_COUNT * 2 + 1;
+                Log.i(TAG,CPU_COUNT+"");
             }
         }.start();
 
@@ -52,6 +61,26 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    private class MyAsyncTask2 extends AsyncTask<Void,Void, Void>
+    {
+
+        @Override
+        protected Void doInBackground(Void... params)
+        {
+            try
+            {
+                Log.e(TAG, Thread.currentThread().getName());
+                Thread.sleep(100);
+            } catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+    }
+
 
 
 }
